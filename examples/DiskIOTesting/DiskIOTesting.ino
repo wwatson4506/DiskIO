36a239b8398603abb061354069d25b2d30fc2ac8
Warrren Watson <wwatson4506@gmail.com>
2021-05-30 15:55:11 -0700

    Add files via upload

--- examples/DiskIOTesting/DiskIOTesting.ino
+++ examples/DiskIOTesting/DiskIOTesting.ino
@@ -4,15 +4,14 @@
 #include "diskIO.h"
 
 diskIO dio;  // One instance of diskIO.
-bool rslt = false;
+//bool rslt = false;
 int br = 0;
-size_t bw = 0;
+int bw = 0;
 char buff[8192]; // Disk IO buffer.
 char sbuff[256]; // readLine buffer.
 
 // A simple read line function.
-char *readLine(char *s)
-{
+char *readLine(char *s) {
 	char *s1 = s;
 	int	c;
 	for(;;) {
@@ -22,9 +21,7 @@ char *readLine(char *s)
 			if(s == s1)
 				break;
 			s1--;
-			Serial.printf("%c",c);
-			Serial.printf(" ");
-			Serial.printf("%c",c);
+			Serial.printf("%c%c%c",c,0x20,c);
 			break;
 		case '\n':
 		case '\r':
@@ -40,8 +37,11 @@ char *readLine(char *s)
 	}
 }
 
-// Change '32GSDFAT32' to a volume name of your drives.
-char *device = "/32GSDFAT32/test1.txt";
+// Change '32GEXFATFD' to the volume name of one of your drives.
+//const char *device = "test1.txt";
+// Or you can specify a logical drive number (partition number)
+// followed with a colon before the path name. 24 patitions are allowed. 0-23.
+const char *device = "20:test1.txt";
 
 void setup()
 {
@@ -53,70 +53,108 @@ void setup()
 
   Serial.printf("%c",12); // Clear screen (VT100).
   Serial.printf(F("DiskIO Testing\r\n\r\n"));
-  Serial.printf(F("Initializing, please wait...\r\n"));
+  Serial.printf(F("Initializing, please wait...\r\n\r\n"));
 
   dio.init();
-
+  // Show a list of mounted partitions.
   Serial.printf(F("\r\nFound and mounted %d logical drives.\r\n"), dio.getVolumeCount());
   dio.listAvailableDrives(&Serial);
-
   Serial.printf(F("\r\n"));
 
-  
+  // Setup string of text to write to a file (test1.txt).
+  sprintf(buff,"%s",(char *)F("This is a test line to test diskIO write() function"));
+
+  // Create an instance of PFsFile.
   PFsFile mscfl; 
 
-  Serial.printf(F("Opening file for write'%s'\r\n"), device);
-  rslt = dio.open(&mscfl,(const char *) device, O_WRONLY | O_CREAT | O_TRUNC);
-  if(!rslt) {
-	Serial.printf(F("open() Failed: s, code %d\r\n\r\n"), device, dio.error(dio.getCDN()));
-  }
+  // Open and create a file for write.
+  Serial.printf(F("Opening this file for write: '%s'\r\n"), device);
+  if(!dio.open(&mscfl,(const char *) device, O_WRONLY | O_CREAT | O_TRUNC))
+	Serial.printf(F("open() Failed: %s, Code: %d\r\n"), device, dio.error());
 
-  char *buff = (char *)F("This is a test line to test diskIO write() function\r\n");
+  // Write our string to the open file.
   bw = dio.write(&mscfl, buff, strlen(buff));
-  buff[bw] = '\0';
-  Serial.printf(F("%u bytes written to file.\r\n"), bw);
-  Serial.printf("%s",buff);
+  if(bw != (int)strlen(buff))
+  	Serial.printf(F("write() Failed: %s Code: %d\r\n\r\n"), device, dio.error());
 
+  // Show the string we wrote to the file and the number of bytes written.
+  Serial.printf("What we wrote: '%s' to: %s\r\n",buff, device);
+  Serial.printf(F("%u bytes written to file.\r\n"), bw);
+
+  // Sync the file.
   Serial.printf(F("flushing buff\r\n"));
   dio.fflush(&mscfl);
 
-  Serial.printf(F("closing file test1.txt\r\n"));
+  //  Close the file.
+  Serial.printf(F("closing file: %s test1.txt\r\n"), device);
   if(!dio.close(&mscfl))
-    Serial.printf(F("close() Failed: %s\r\n\r\n"), device, rslt);
+    Serial.printf(F("close() Failed: %s\r\n\r\n"), device, dio.error());
 
-  Serial.printf(F("Opening file for read'%s'\r\n"), device);
+  // Re-open same file for read.
+  Serial.printf(F("Opening this file for read: '%s'\r\n"), device);
   if(!dio.open(&mscfl, (const char *)device, O_RDONLY))
-	Serial.printf(F("open() Failed: %s Code: %d\r\n\r\n"), device, dio.error(dio.getCDN()));
+	Serial.printf(F("open() Failed: %s Code: %d\r\n\r\n"), device, dio.error());
 
-  Serial.printf(F("Seeking to file position %d\r\n"),dio.lseek(&mscfl, 10, SEEK_SET));
-  Serial.printf(F("ftell returned %d\r\n"), dio.ftell(&mscfl));
+  // Seekfile to position 10.
+  Serial.printf(F("Seeking to file position 10\r\n"));
+  if(!dio.lseek(&mscfl, 10, SEEK_SET))
+  	Serial.printf(F("lseek() Failed: %s Code: %d\r\n\r\n"), device, dio.error());
 
-  br = dio.read(&mscfl, buff, 8192);
+  // Check the current file position.
+  Serial.printf(F("Getting current file position\r\n"));
+  if(!dio.ftell(&mscfl))
+  	Serial.printf(F("ftell() Failed: %s Code: %d\r\n\r\n"), device, dio.error());
+  else
+  	Serial.printf(F("ftell() returned %d\r\n"), dio.ftell(&mscfl));
+
+  // Read in the line of text.    
+  if((br = dio.read(&mscfl, buff, 8192)) < 0)
+  	Serial.printf(F("read() Failed: %s Code: %d\r\n\r\n"), device, dio.error());
   buff[br] = '\0';
-  Serial.printf(F("%sbr = %d\r\n\r\n"),buff, br);
+  
+  // Show the line and the number of bytes read.
+  Serial.printf(F("What we read '%s' from %s\r\n"), buff, device);
+  Serial.printf(F("bytes read = %d\r\n"), br);
 
+  //  Close the file.
   if(!dio.close(&mscfl))
-    Serial.printf(F("close() Failed: %s\r\n\r\n"), device, rslt);
-  dio.lsDir((char *)device);
+    Serial.printf(F("close() Failed: %s code: %d\r\n\r\n"), device, dio.error());
+
+  // List the directory of device.
+  Serial.printf(F("\r\n"));
+  if(!dio.lsDir((char *)device))
+	Serial.printf(F("lsDir() Failed: %s, Code: %d\r\n\r\n"), device, dio.error());
+  Serial.printf(F("Press enter to continue...\r\n"));
+  readLine((char *)sbuff);
 }
 
 void loop(void) {
 
-
-  readLine((char *)sbuff);
-
+  Serial.printf(F("\r\n********************************************************\r\n"));
+  Serial.printf("Unplug a USB drive then press enter.\r\n");
+  Serial.printf("Plug the same USB drive in then press enter again.\r\n");
+  Serial.printf("This shows hot plugging at work. USB drives only so far.\r\n");
+  Serial.printf("Four USB drives are supported (SD's not yet).\r\n");
+  Serial.printf(F("********************************************************\r\n\r\n"));
+  
   Serial.printf(F("checking for device #%d\r\n"), 0);
   if(!dio.isConnected(0))
-    Serial.printf(F("USB Drive 0 not connected: Code 0x%2.2x\r\n"),dio.error(0));
+    Serial.printf(F("USB Drive 0 not connected: Code 0x%2.2x\r\n"),dio.error());
 
   Serial.printf(F("checking for device #%d\r\n"), 1);
   if(!dio.isConnected(1))
-    Serial.printf(F("USB Drive 1 not connected: Code 0x%2.2x\r\n"),dio.error(1));
+    Serial.printf(F("USB Drive 1 not connected: Code 0x%2.2x\r\n"),dio.error());
 
   Serial.printf(F("checking for device #%d\r\n"), 2);
   if(!dio.isConnected(2))
-    Serial.printf(F("USB Drive 2 not connected: Code 0x%2.2x\r\n"),dio.error(2));
+    Serial.printf(F("USB Drive 2 not connected: Code 0x%2.2x\r\n"),dio.error());
+
+  Serial.printf(F("checking for device #%d\r\n"), 3);
+  if(!dio.isConnected(3))
+    Serial.printf(F("USB Drive 3 not connected: Code 0x%2.2x\r\n"),dio.error());
 
   dio.listAvailableDrives(&Serial);
+  Serial.printf(F("Press enter to continue...\r\n"));
+  readLine((char *)sbuff);
  
 }
