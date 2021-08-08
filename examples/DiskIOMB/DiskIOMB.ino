e4fe96f61de5e64cfcf127e521ffe76f8f8b5207
Warrren Watson <wwatson4506@gmail.com>
2021-08-08 14:26:20 -0700

    Add files via upload

--- examples/DiskIOMB/DiskIOMB.ino
+++ examples/DiskIOMB/DiskIOMB.ino
@@ -1,12 +1,14 @@
+// DiskIOMB
+// This is a version of microBox hacked for testing the diskIO library.
+
 #include "Arduino.h"
 #include "diskIOMB.h"
 #include "mscFS.h"
 #include "diskIO.h"
-#include "T4_PowerButton.h"
-
+#include "T4_PowerButton.h" // To get free mem left.
 
 char historyBuf[100];
-char hostname[] = "T4.1";
+char hostname[] = "Teensy";
 
 PARAM_ENTRY Params[]=
 {
@@ -14,11 +16,13 @@ PARAM_ENTRY Params[]=
   {NULL, NULL}
 };
 
+// Added for diskIO
 void getMillis(char **param, uint8_t parCnt)
 {
   Serial.println(millis());
 }
 
+// Added for diskIO
 void freeRam(char **param, uint8_t parCnt) 
 {
   Serial.println(memfree());
@@ -29,13 +33,17 @@ void setup()
    while (!Serial) {
     SysCall::yield(); // wait for serial port to connect.
   }
-
-  // This line is use with VT100 capable terminal program.
+  
+  if(CrashReport)
+	Serial.print(CrashReport);
+  
+  // This line works only with VT100 capable terminal program.
   Serial.printf("%c",12); // Clear screen (VT100).
 
+  Serial.printf(F("DiskIOMB\r\n\r\n"));
+  Serial.printf(F("The original version of microBox found here:\r\n"));
+  Serial.printf(F(" http://sebastian-duell.de/en/microbox/index.html\r\n\r\n"));
   Serial.printf(F("Initializing, please wait...\r\n\r\n"));
-
-  
   microbox.begin(&Params[0], hostname, true, historyBuf, 100);
   microbox.AddCommand("free", freeRam);
   microbox.AddCommand("millis", getMillis);
@@ -43,5 +51,5 @@ void setup()
 
 void loop()
 { 
-  microbox.cmdParser();
+  microbox.cmdParser(); // Monitor cmd input.
 }
