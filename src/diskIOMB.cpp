74a5bfe63ebd1c4c5ad90f86604155ba69c3eeeb
Warrren Watson <wwatson4506@gmail.com>
2021-08-29 13:56:31 -0700

    Update diskIOMB.cpp

--- src/diskIOMB.cpp
+++ src/diskIOMB.cpp
@@ -1096,7 +1096,7 @@ void microBox::help(char** pParam, uint8_t parCnt)
 	Serial.printf(F("rename - Rename file or directory.\r\n"));
 	Serial.printf(F("cp     - Copy file (src dest).\r\n"));
 	Serial.printf(F("cat    - List file (Ascii only).\r\n"));
-	Serial.printf(F("play    - Play a Wav file. Press 'end' key to stop.\r\n"));
+	Serial.printf(F("play    - Play a Wav file. Press 'end' key to stop.(VT100 Terminal)\r\n"));
 	Serial.printf(F("        - Cannot perform disk operation on same device WAV file \r\n"));
 	Serial.printf(F("        - is playing from. It will lock up DiskIOMB!\r\n\r\n"));
 	Serial.printf(F("All commands except clear and ld accept an optional drive spec.\r\n"));
