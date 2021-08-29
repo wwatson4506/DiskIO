0535685ac400090e5a78464211bd4cf334089b49
Warrren Watson <wwatson4506@gmail.com>
2021-08-29 12:58:43 -0700

    Major updates. Add play wave file functions.

--- examples/DiskIOMB/DiskIOMB.ino
+++ examples/DiskIOMB/DiskIOMB.ino
@@ -3,12 +3,14 @@
 
 #include "Arduino.h"
 #include "diskIOMB.h"
-#include "mscFS.h"
 #include "diskIO.h"
+#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40)
 #include "T4_PowerButton.h" // To get free mem left.
+#endif
 
 char historyBuf[100];
 char hostname[] = "Teensy";
+
 
 PARAM_ENTRY Params[]=
 {
@@ -22,11 +24,13 @@ void getMillis(char **param, uint8_t parCnt)
   Serial.println(millis());
 }
 
+#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40)
 // Added for diskIO
 void freeRam(char **param, uint8_t parCnt) 
 {
   Serial.println(memfree());
 }
+#endif
 
 void setup()
 {
@@ -34,8 +38,10 @@ void setup()
     SysCall::yield(); // wait for serial port to connect.
   }
   
+#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40)
   if(CrashReport)
 	Serial.print(CrashReport);
+#endif
   
   // This line works only with VT100 capable terminal program.
   Serial.printf("%c",12); // Clear screen (VT100).
@@ -44,8 +50,11 @@ void setup()
   Serial.printf(F("The original version of microBox found here:\r\n"));
   Serial.printf(F(" http://sebastian-duell.de/en/microbox/index.html\r\n\r\n"));
   Serial.printf(F("Initializing, please wait...\r\n\r\n"));
+  Serial.printf(F("Type 'help' for a list of commands...\r\n\r\n"));
   microbox.begin(&Params[0], hostname, true, historyBuf, 100);
+#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40)
   microbox.AddCommand("free", freeRam);
+#endif
   microbox.AddCommand("millis", getMillis);
 }
 
